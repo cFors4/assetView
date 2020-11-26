3c551ccae0b1e4b7d7d971c638e4bd5be2bffa3c
Connor Forsyth <connorsforsyth@gmail.com>
2020-11-26 20:34:07 +0000

    coinbase pro workaround

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import time
 from datetime import date
 
-import pandas as pd
-from IPython.core.display import HTML
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from secrets import *
+
+import requests
 import json
 
-
-#Loads the driver
 def load_driver():
     chrome_options = Options()
 
     #chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--disable-web-security")
+    chrome_options.add_argument("--allow-running-insecure-content")
 
     driver = webdriver.Chrome(options = chrome_options)
 
     return driver
 
-#Login page
 def login212(driver, url, username, password):
     driver.get(url)
     time.sleep(5)
@@ -47,47 +45,65 @@ def getdata212(driver):
     profit = driver.find_element_by_xpath("//*[@id='app']/div[1]/div[2]/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div[2]/div[2]")
     profit = profit.text
 
-    print(total)
     profitData = profit.split()
 
     netProfit = profitData[0][2:len(profitData[0])-3].replace(',','')
-    print(netProfit)
     pecentageProfit = profitData[1][1:len(profitData[1])-1].replace('%','')
-    print(pecentageProfit)
 
     cash = driver.find_element_by_xpath("//*[@id='app']/div[1]/div[1]/div/div[2]/div[1]/div/span[2]")
     cash = cash.text
     cash = int(cash[1:len(cash)-3].replace(',',''))-int(total)
-    print(cash)
     
-    return total
-        
-def plot():
-    pass
+    return total,int(netProfit),float(pecentageProfit),cash
+            
+def getdataPro(driver,url):
+    print("coinbase pro protfolio")
+
+    driver.get(url)
+    time.sleep(5)                              
+    fetch_field = driver.find_element_by_xpath('//*[@id="t-formula-bar-input"]/div').text
+    BTC = float(fetch_field)
+
+    bitcoin_api_url = 'https://www.google.com/search?q=bitcoin+price&oq=bitocin+pri&aqs=chrome.1.69i57j0i10i131i433i457j0i10i131i433l6.4397j1j4&sourceid=chrome&ie=UTF-8'
+    driver.get(bitcoin_api_url)
+    time.sleep(5)  
+    BTC_field = driver.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').text
+    BTC_price = float(BTC_field.replace(',',''))
+    urlcash = "https://docs.google.com/spreadsheets/d/1Gm9oqiUFgddTm33flhOMajKeMZaAQYT7cSMHBvYdpJY/edit?folder=1AUaHi4waMcvD3rfZl6DmNX9PSFNUfro5#gid=958840544&range=D9"
+    driver.get(urlcash)
+    time.sleep(5)                              
+    cash_field = driver.find_element_by_xpath('//*[@id="t-formula-bar-input"]/div').text
+    cash = float(cash_field)
+
+    return BTC_price*BTC,cash
+
     
 def main():
     today = date.today()
-    url = "https://live.trading212.com/beta"
-
+    url212 = "https://live.trading212.com/beta"
+    urlPro = "https://pro.coinbase.com/"
+    urlGooglePro = "https://docs.google.com/spreadsheets/d/1Gm9oqiUFgddTm33flhOMajKeMZaAQYT7cSMHBvYdpJY/edit?folder=1AUaHi4waMcvD3rfZl6DmNX9PSFNUfro5#gid=958840544&range=D8"
+    
     username = "connorsforsyth@gmail.com"
     password = password212
 
     driver = load_driver()
 
     # while True: keep updating
-    login212(driver, url, username, password)
+    #TRADING212
+    login212(driver, url212, username, password)
     time.sleep(20)
-    data212 = getdata212(driver)
-    # loginPro
-    # time.sleep(20)
-    # dataPro = getdataPro()
-    driver.get_screenshot_as_file("screenshot.png")
+    total212,netProfit,pecentageProfit,cash = getdata212(driver)
+    driver.get_screenshot_as_file("trading212.png")
+    print(total212,netProfit,pecentageProfit,cash)
+
+    #BITCOIN
+    totalPro,cash = getdataPro(driver,urlGooglePro)
+    print(totalPro,cash)
+    
     # plot data
     
-    
-    
-
-    # driver.close()
+    driver.close()
     driver.quit()
 
 if __name__ == "__main__":
